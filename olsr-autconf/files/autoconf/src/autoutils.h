2f9d84c12b092f53e4b3d3010ee3e51abc27865a
oraziopiratadellospazio <oraziopiratadellospazio@ef3c9aa7-e928-0410-9a28-8491e62b674b>
2009-03-21 18:56:47 +0000

    added utils files
    git-svn-id: https://svn.ninux.org/svn/ninuxdeveloping/packages@661 ef3c9aa7-e928-0410-9a28-8491e62b674b

---
+++ olsr-autconf/files/autoconf/src/autoutils.h
@@ -0,0 +1,23 @@
+#ifdef OLSR_AUTOCONF_AUTOUTILS
+#define OLSR_AUTOCONF_AUTOUTILS
+
+/* Configure Interfaces (others than if2skip) in ad-hoc mode and configure
+ * Ipv4 address to saved_if (if any) or to some bytes taken from mac hardware.
+ */
+
+int autoconf_interfaces(char* if2skip, char* saved_if){
+
+
+return 0;
+}
+
+/* Change the interface address upon a collision is detected
+ */
+
+int change_if(char* if){}
+
+
+
+
+
+#endif
