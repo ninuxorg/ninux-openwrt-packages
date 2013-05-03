2f6fec4fbebb3606b15b6a1e0434555c75b95701
Saverio Proto <zioproto@gmail.com>
2013-05-04 01:10:22 +0200

    reghack: unload and reload modules after binary patch, you dont need to reboot

--- reghack/src/reghack.sh
+++ reghack/src/reghack.sh
@@ -10,3 +10,14 @@ do
 	/usr/bin/reghack ${module}.ko
 	mv ${module}.ko /lib/modules/*/
 done
+
+rmmod ath9k ath9k_common ath9k_hw ath mac80211 cfg80211 compat
+
+insmod compat
+insmod cfg80211 
+insmod mac80211
+insmod ath 
+insmod ath9k_hw 
+insmod ath9k_common 
+insmod ath9k
+
